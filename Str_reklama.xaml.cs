a7143714e8109e2d8c5e5cbfe045d3a46d1fb6d7
Анастасия Курсанова <lynn43521@gmail.com>
2021-12-03 14:50:01 +0300

    14 практика

---
+++ Str_reklama.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Str_reklama.xaml
+    /// </summary>
+    public partial class Str_reklama : Page
+    {
+        public Str_reklama()
+        {
+            InitializeComponent();
+        }
+    }
+}
